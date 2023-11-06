ce718ef4dfd7838045f1048ab2c57f65fcd72b0b
yogita <yogita@yogitas-mbp.home>
2023-11-06 22:49:51 +0000

    Refactor and optimise code

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,8 +1,10 @@
 # import libraries
+# import libraries 
 import random
+
 # Milestone_3: Task 3
 # Check weather the guess is in the word
-def check_guess(guess):
+def check_guess_in_word(guess):
     guess = guess.lower()
     secret_word = ("apple", "spoon", "easy", "sown", "help")
     random_word = random.choice(secret_word)
@@ -11,22 +13,22 @@ def check_guess(guess):
     else:
         print(f'Sorry, {guess} is not in the word. Try again.')
                 
-guess = input("Guess a letter: ")
-check_guess(guess)
-
 
 # Iteratively check if input is valid and check if the input letter is in the random word
-def ask_for_input():
+def check_input_validity_n_guess():
+     # while loop
      while True:
         guess = input("Guess a letter: ")
         if len(guess)== 1 and guess.isalpha():
-            check_guess(guess)
+            # Check the valid letter is in the word
+            check_guess_in_word(guess)
             break
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
             continue
 
 # Call function
-ask_for_input()
+check_input_validity_n_guess()
+
 
 

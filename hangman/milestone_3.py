efc7c24105b6d66022fa1cfaeff8b1cf74a9aa80
yogita <yogita@yogitas-mbp.home>
2023-11-06 22:08:01 +0000

    Milestone 3: Task 3 completed

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,25 +1,32 @@
 # import libraries
 import random
-
-# Task 1: Iteratively check if input is valid
-
-while True:
-    guess = input("Guess a letter: ")
-    if len(guess)== 1 and guess.isalpha():
-        letter = guess
-        break
+# Milestone_3: Task 3
+# Check weather the guess is in the word
+def check_guess(guess):
+    guess = guess.lower()
+    secret_word = ("apple", "spoon", "easy", "sown", "help")
+    random_word = random.choice(secret_word)
+    if guess in list(random_word):
+        print(f'Good guess! {guess} is in the word.')
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-        continue
+        print(f'Sorry, {guess} is not in the word. Try again.')
+                
+guess = input("Guess a letter: ")
+check_guess(guess)
 
 
-# Task 2: check weather the guess is in the word
+# Iteratively check if input is valid and check if the input letter is in the random word
+def ask_for_input():
+     while True:
+        guess = input("Guess a letter: ")
+        if len(guess)== 1 and guess.isalpha():
+            check_guess(guess)
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+            continue
 
-secret_word = ("apple", "spoon", "easy", "sown", "help")
-random_word = random.choice(secret_word)
+# Call function
+ask_for_input()
 
-if guess in list(random_word):
-    print(f'Good guess! {guess} is in the word.')
-else:
-    print(f'Sorry, {guess} is not in the word. Try again.')
-            
+

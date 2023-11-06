5a18c3f6db320e1845d88f1c3c40bcd2c39a5ff8
yogita <yogita@yogitas-mbp.home>
2023-11-06 01:29:02 +0000

    Task 2: Completed

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,4 +1,12 @@
+# import random
+import random as rd
+
+# Task 1
 # List containing the names of my 5 favourite fruits
 word_list = ['strawberry', 'kiwi', 'grapes', 'banana', 'orange']
 # Print out the created list
 print(word_list)
+
+# Task 2
+word = rd.choice(word_list)
+print(word) 

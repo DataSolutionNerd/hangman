662ae25ffa882a00de2057244b0501198a090e8f
yogita <yogita@yogitas-mbp.home>
2023-11-06 04:27:34 +0000

    Task 2: completes and simplifies task one to access guess

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,12 +1,29 @@
-# Task 1: Iteratively check if input is valid
-def checks():
-    while True:
-        guess = input("Guess a letter: ")
-        if len(guess)== 1 and guess.isalpha():
-            break
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-            continue
-    return guess
+import random
 
-checks()
+# Task 1: Iteratively check if input is valid
+
+while True:
+    guess = input("Guess a letter: ")
+    if len(guess)== 1 and guess.isalpha():
+        letter = guess
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+        continue
+
+
+
+
+
+# Task 2: check weather the guess is in the word
+
+import random
+
+secret_word = ("apple", "spoon", "easy", "sown", "help")
+random_word = random.choice(secret_word)
+
+if guess in list(random_word):
+    print(f'Good guess! {guess} is in the word.')
+else:
+    print(f'Sorry, {guess} is not in the word. Try again.')
+            

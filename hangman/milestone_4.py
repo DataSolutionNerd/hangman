151bb5019c6e39c5759297014a99aa0188234f9a
yogita <yogita@yogitas-mbp.home>
2023-11-09 23:31:25 +0000

    Refactor and optimise current code, fixed some bugs too

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -3,35 +3,33 @@ import random as rd
 
 class Hangman:
     # constructor
-    def __init__(self, word_list):
+    def __init__(self, word_list, num_lives ):
         # initialise the attributes
         self.word_list = word_list
         self.num_lives = 5
         self.word = rd.choice(self.word_list)
         self.word_guessed = ['_'] * len(self.word)
-        self.num_letters = len(set(self.word)) - len(set(list(filter(lambda x: x != '_', self.word_guessed))))
-        self.list_of_guesses = list()
+        self.num_letters = len(set(self.word))
+        self.list_of_guesses = []
         
+    # def print_all_attributes(self):
+       #  print(f'word_list: {self.word_list}, num_lives: {self.num_lives}, word: {self.word}, word_guessed: {self.word_guessed}, num_letters: {self.num_letters}, list_of_guesses: {self.list_of_guesses}')
 
     def check_guess(self, guess):
-        if guess.lower() in list(self.word):
-            print("Good guess! {guess} is in the word.")
-            position = 0
-            for x  in list(self.word):
-                if x.lower == guess.lower():
-                    self.word_guessed[position] == guess
-                    position = position+1      
-                else:
-                    position = position+1       
+        guess = guess.lower()
+        if guess in list(self.word):
+            self.num_letters = self.num_letters- 1  
+            for i, x in enumerate(self.word):
+                if x == guess:
+                    self.word_guessed[i] = x
+            print(f"Good guess! {guess} is in the word.")
+            print(f"{self.word_guessed}")
         else:
             self.num_lives = self.num_lives - 1
-            print("Sorry, {letter} is not in the word.")
-            print("You have {self.num_lives} lives left.")   
-        self.num_letters = self.num_letters- 1
-
-
-
-
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+            print(f"{self.word_guessed}")
+        
 
 
     def ask_for_input(self):
@@ -39,20 +37,36 @@ class Hangman:
             guess = input("Guess a letter: ")
             if len(guess) != 1 or guess.isalpha() == False:
                 print("Invalid letter. Please, enter a single alphabetical character.")
+                continue
             elif guess in self.list_of_guesses:
-                print("You already tried that letter!")
-            else:
-                mileston_four.check_guess(guess)
+                print(f"You already tried the letter {guess}!")
                 self.list_of_guesses.append(guess)
+                continue
+            else:
+                #self.list_of_guesses.append(guess)
+                self.check_guess(guess)
+            break
+       
+def play_game(word_list):
+    game = Hangman(word_list, num_lives=5)
+    while True:
+        if game.num_lives == 0:
+            print('You lost!')
+            break
+        elif '_' not in game.word_guessed:
+            print('Congratulations. You won the game!')
+            break
+        elif game.num_lives>0:
+            game.ask_for_input()
+        elif game.num_lives != 0 and game.num_letters>0 :
+            game.ask_for_input()
+        elif game.num_lives != 0 and game.num_letters <0 :
+            print('Congratulations. You won the game!')
+            break
+        
 
-
-# Test Code 
-secret_word = ["apple", "spoon", "easy", "sown", "help"]
-mileston_four = Hangman(secret_word)
-mileston_four.ask_for_input()
-  
-
-
-
-
-
+if __name__ == '__main__':
+    word_list = ["apple", "spoon", "money", "green", "help"]
+    play_game(word_list)
+        
+        
--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -1,23 +1,34 @@
-from milestone_4 import Hangman
+import milestone_4
+
 class milestone_5:
+    
     def play_game(word_list):
         num_lives = 5
-        game = Hangman()
-        game(word_list,num_lives)
+        game = milestone_4.Hangman(word_list, num_lives)
         while True:
             if num_lives == 0:
                 print('You lost!')
-            if num_lives>0:
+                break
+            elif num_lives>0:
                 game.ask_for_input()
-            if num_lives != 0 and game.num_letters>0 :
+            elif num_lives != 0 and game.num_letters>0 :
                 game.ask_for_input()
-            if num_lives != 0 and game.num_letters <0 :
+            elif num_lives != 0 and game.num_letters <0 :
                 print('Congratulations. You won the game!')
+                break
 
 
-milestone_5.play_game()
+# Test Code 
+word_list = ["apple", "spoon", "easy", "sown", "help"]
 
+game_start = milestone_5()
+game_start.play_game(word_list)
 
+# Test Code 
+# secret_word = ["apple", "spoon", "easy", "sown", "help"]
+# mileston_four = Hangman(secret_word)
+# mileston_four.print_all_attributes()
+# ileston_four.ask_for_input()
 
 
 
